c5c18af47e4b7ec2629f5a5681504ecc165d45a5
mohamed-mahfouz14 <mohamed.mahfouz944@gmail.com>
2021-07-07 13:41:23 +0200

    first commit

---
+++ cartProject/main.js
@@ -0,0 +1,72 @@
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [
+    {
+        name: 'Grey Tshirt',
+        tag: 'greytshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Sleeves',
+        tag: 'sleeves',
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: 'Hoodi',
+        tag: 'hoodi',
+        price: 28,
+        inCart: 0
+    }
+];
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+    setItems(product);
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems != null) {
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    
+        console.log('My cartItems are', cartItems);
+
+    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+}
+
+onLoadCartNumbers();
